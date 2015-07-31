bedc59301650ab2d64246902ba845850bafcf9fd
neilellis <hello@neilellis.me>
2015-07-31 23:34:06 +0100

    Initial work

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 cd server
 mvn clean install -Dmaven.test.skip=true
 docker pull sillelien/java-slim
-docker run -ti -v /var/run:/var/run -v $(pwd)/:/build/ sillelien/java-slim sillelien/tutum-scheduler scheduler target/tutum-scheduler-1.0-SNAPSHOT-jar-with-dependencies.jar src/main/resources sillelien.scheduler.Main
+docker run -ti -v /var/run:/var/run -v $(pwd)/:/build/ sillelien/java-slim sillelien/scheduler scheduler target/tutum-scheduler-1.0-SNAPSHOT-jar-with-dependencies.jar src/main/resources sillelien.scheduler.Main
 cd -
